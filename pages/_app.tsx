c0b52055ce509bdcc7dc47f9cf9b77e5e854b986
shinyo17 <17shinyo17@gmail.com>
2023-05-24 22:56:22 +0900

    init: 프로젝트 뼈대 head 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>SKZ 취향표</title>
+        <meta name="description" content="SKZ 취향표" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:title" content="SKZ 취향표" />
+        <meta property="og:description" content="SKZ 취향표" />
+        <meta
+          property="og:image"
+          content="https://skz-style.vercel.app/images/meta.png"
+        />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:url" content="https://skz-style.vercel.app/" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="SKZ 취향표" />
+        <meta property="twitter:url" content="https://skz-style.vercel.app/" />
+        <meta property="twitter:description" content="SKZ 취향표" />
+        <meta
+          property="twitter:image"
+          content="https://skz-style.vercel.app/images/meta.png"
+        />
+      </Head>
+      <div className="font-suite">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
